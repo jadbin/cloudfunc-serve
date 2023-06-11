19e2d8feaf81701f8ca127f7e8dd00a9830e3aa2
jadbin <jadbin.com@hotmail.com>
2023-06-11 17:10:57 +0800

    update

--- resources/serve.py
+++ resources/serve.py
@@ -1,25 +1,23 @@
 # coding=utf-8
 
-import sys
-from typing import List
-from os.path import abspath, dirname, join
 import socket
-from pip._internal.cli.main import main as pip_main
+import sys
+from os.path import abspath, dirname, join
+from typing import List
 
-from cloudfunc.setuptools import run_setup, after_setup
 from cloudfunc.app import create_app
+from cloudfunc.setuptools import run_setup, after_setup
 from gevent.pywsgi import WSGIServer
+from pip._internal.cli.main import main as pip_main
 
 home = dirname(abspath(__file__))
 sys.path.append(home)
-
-pip_index = 'https://pypi.doubanio.com/simple/'
 
 
 def install_requirements(install_requires: List[str]):
     if not install_requires:
         return
-    cmd = ['install'] + install_requires + ['--index', pip_index]
+    cmd = ['install'] + install_requires + ['--index', after_setup['pypi_url']]
     pip_main(cmd)
 
 
@@ -48,9 +46,11 @@ def main():
     install_requirements(after_setup['install_requires'])
     name = after_setup['name']
     port = get_free_port()
-    app = create_app(name,
-                     settings=default_settings(name, port),
-                     includes=after_setup['includes'])
+    app = create_app(
+        name,
+        settings=default_settings(name, port),
+        includes=after_setup['includes'],
+    )
     wsgi = WSGIServer(('0.0.0.0', port), application=app)
     wsgi.serve_forever()
 
